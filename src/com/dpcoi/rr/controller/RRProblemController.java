fa16f0d0e26c7427d86a85284a58d08a2b2c5976
liangzhifu <1009568392@qq.com>
2018-07-04 15:39:25 +0800

    荆州RR问题点查询修改

--- src/com/dpcoi/rr/controller/RRProblemController.java
+++ src/com/dpcoi/rr/controller/RRProblemController.java
@@ -172,6 +172,9 @@ public class RRProblemController {
             if(speedOfProgress == null || "".equals(speedOfProgress)){
                 speedOfProgress = "delayI,delayII,delayIII,delayIV,follow";
                 rrProblemQuery.setSpeedOfProgress(speedOfProgress);
+            } else if (speedOfProgress.indexOf("open") >= 0) {
+                speedOfProgress += ",delayI,delayII,delayIII,delayIV,follow";
+                rrProblemQuery.setSpeedOfProgress(speedOfProgress);
             }
             String path = request.getSession().getServletContext().getRealPath("/");
             String fileName = this.exportExcelService.excelRRProblem(path, rrProblemQuery);
@@ -202,6 +205,9 @@ public class RRProblemController {
             String speedOfProgress = rrProblemQuery.getSpeedOfProgress();
             if(speedOfProgress == null || "".equals(speedOfProgress)){
                 speedOfProgress = "delayI,delayII,delayIII,delayIV,follow";
+                rrProblemQuery.setSpeedOfProgress(speedOfProgress);
+            } else if (speedOfProgress.indexOf("open") >= 0) {
+                speedOfProgress += ",delayI,delayII,delayIII,delayIV,follow";
                 rrProblemQuery.setSpeedOfProgress(speedOfProgress);
             }
             List<Map<String, Object>> rrProblemList = this.rRProblemService.queryRRProblemPageList(rrProblemQuery);
